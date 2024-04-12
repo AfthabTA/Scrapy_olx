38b6ec556ddfdaf06ca2bf334a2f0f721cbd9f78
AfthabTA <afthabt85@gmail.com>
2024-04-12 15:03:31 +0530

    seventh commit

--- olxscraper/olxscraper/spiders/olxspider.py
+++ olxscraper/olxscraper/spiders/olxspider.py
@@ -2,94 +2,11 @@ import scrapy
 # from scrapy.spiders import CrawlSpider,Rule
 # from scrapy.linkextractors import LinkExtractor
 import json
-from scrapy_splash import SplashRequest
+# from scrapy_splash import SplashRequest
+# import requests
+# from helium import *
+# from bs4 import BeautifulSoup
 
-
-# class olxspider(scrapy.Spider):
-#     name = 'olx'
-#     start_urls=['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
-
-#     def parse(self,response):
-        
-#         for products in response.css('li._1DNjI'):
-#             link=products.css('a._2cbZ2').attrib['href']
-#             if link is not None:
-#                 yield response.follow(link)
-
-#                 yield {
-#                     'name': products.css('h1._1hJph::text').get(),
-
-#                     'price': products.css('span.T8y-z::text').get().replace('₹',''),
-
-#                     'location': products.css('span._1RkZP::text').get(),
-                    
-
-#                 }
-
-# https://www.olx.in/item/sree-pottammal-iid-1764531561
-# https://www.olx.in/item/3bhk-luxury-furnished-flat-for-rent-at-thondayad-calicut-md-iid-1765792144
-            
-# class olxspider(CrawlSpider):
-#         name = 'olx'
-
-#         allowed_domains=['olx.in']
-
-#         start_urls=['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723']
-
-#         headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'}
-
-#         rules =(
-#                 Rule(LinkExtractor(allow='item'),callback='parse_item'),
-#         )
-
-#         def parse_item(self,response):
-           
-#                 price=response.css('span.T8y-z::text').get()
-#                 if print is not None:
-#                         spl_price=price.split()
-#                         if len(spl_price)>=2:
-#                                 currency=spl_price[0]
-#                                 amount=spl_price[-1]
-#                                 price_dic={'amount':amount,'currency':currency,}
-
-
-#                 yield{
-#                         'property_name': response.css('h1._1hJph::text').get(),
-
-#                         'propery_id't: response.css('div._1-oS0 strong::text')[-1].get(),
-
-#                         'breadcrumbs': response.css('ol.rui-2Pidb li a::text').getall(),
-
-#                         'price': price_dic,
-
-#                         'image_url': response.xpath('//img[@data-aut-id="defaultImg"]/@src').get(),
-
-#                         'description': response.xpath('//div[@data-aut-id="itemDescriptionContent"]/p/text()').getall(),
-
-#                         'seller_name': response.css('div.eHFQs::text').get(),
-
-#                         'location': response.css('span._1RkZP::text').get(),
-
-#                         'property_type': response.xpath('//span[@data-aut-id="value_type"]/text()').get(),
-
-#                         'bathrooms': response.xpath('//span[@data-aut-id="value_bathrooms"]/text()').get(),
-
-#                         'bedrooms': response.xpath('//span[@data-aut-id="value_rooms"]/text()').get(),
-                        
-
-                        
-#                 }
-
-#                 page=2
-#                 while page<=300:
-#                         next_page='https://www.olx.in/api/relevance/v4/search?category=1723&facet_limit=100&lang=en-IN&location=4058877&location_facet_limit=20&platform=web-desktop&relaxedFilters=true&size=40&user=023995345675097202&page={page}'
-
-#                         yield response.follow(next_page, callback=self.parse_item)
-
-# https://www.olx.in/api/relevance/v4/search?category=1723&facet_limit=100&lang=en-IN&location=4058877&location_facet_limit=20&page=2&platform=web-desktop&relaxedFilters=true&size=40&user=023995345675097202
-# https://www.olx.in/api/relevance/v4/search?category=1723&facet_limit=100&lang=en-IN&location=4058877&location_facet_limit=20&page=3&platform=web-desktop&relaxedFilters=true&size=40&user=023995345675097202
-# https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page=2     
-# https://www.olx.in/api/relevance/v4/search?category=1723&facet_limit=100&lang=en-IN&location=4058877&location_facet_limit=20&platform=web-desktop&relaxedFilters=true&size=40&user=023995345675097202&page=2
 
 
 
@@ -126,12 +43,7 @@ class olxspider(scrapy.Spider):
             for i in range(len(images)):
                   list_images.append(images[i]['url'])
 
-            # owner=property['user_id']   
-            # profile_url='https://www.olx.in/profile/'+owner
-            # profile =SplashRequest(url=profile_url)
-            # seller_name=profile.xpath('//div[@class="_31kC9"]/text()').get()
-            # yield seller_name         
-            # yield SplashRequest(profile_url, self.parse_owner)  
+           
 
                               
        
@@ -162,13 +74,17 @@ class olxspider(scrapy.Spider):
             
             yield item
 
+            # owner=property['user_id']   
+            # profile_url='https://www.olx.in/profile/'+owner
+
+            # browser= start_chrome(profile_url, headless=True)
+            # soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+            # name=soup.find('div',{'class':'_31kC9'}).Text
+            # print(name)
+
             next_page= data['metadata']['next_page_url']
         if next_page is not None:
                 next_page=response.urljoin(next_page)
                 yield scrapy.Request(next_page, callback=self.parse)
 
-  
-
-    # def parse_owner(self, response):
-    #           seller_name=response.xpath('//div[@class="_31kC9"]/text()').get()
-    #           yield seller_name
